aa918095a123a285d4535667bc16d8236c4b44b3
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-07-08 14:11:52 -0400

    Api not working fully

---
+++ bookstoreproject/src/components/BookCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+const BookCard = (props) => {
+    const  book  = props.book;
+
+    return(
+        <div className="card-container">
+            <img src="https://cdn5.vectorstock.com/i/1000x1000/76/54/vintage-retro-book-cover-design-vector-16217654.jpg" alt="" />
+            <div className="desc">
+                <h2>
+                    <Link to={`/show-book/${book._id}`}>
+                        { book.title }
+                    </Link>
+                </h2>
+                <h3>{book.author}</h3>
+                <p>{book.description}</p>
+            </div>
+        </div>
+    )
+};
+
+export default BookCard;
